c51ec20938208f9abd9e9242a8679fe850f4bacd
ABHISHEK30-TECH <abhishekumar4165@gmail.com>
2025-02-10 20:53:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,42 @@
 //complete this code
-class Person {}
+class Person {
+	constructor(name,age){
+		this.n = name;
+		this.a = age;
+	}
+}
+get name(){
+	return this.n;
+}
 
-class Student extends Person {}
+get age(){
+	return this.a;
+}
 
-class Teacher extends Person {}
+set newName(){
+	return this.newName = newName;
+}
+
+set age(newAge){
+	if(newAge > 0){
+	this.age=newAge;
+	}
+	else{
+		console.error("Age must be positive number");
+	}
+}
+
+class Student extends Person {
+	study() {
+    return `${this.n} is studying.`;
+  }
+}
+
+class Teacher extends Person {
+	 teach() {
+    return `${this.n} is teaching.`;
+  }
+}
 
 // Do not change the code below this line
 window.Person = Person;
